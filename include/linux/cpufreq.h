1cf2d3184802460bdaabf8297856c8c02784f2fa
engstk <eng.stk@sapo.pt>
2014-12-15 13:40:54 +0000

    cpufreq: Add & Enable BLU_ACTIVE governor
    Signed-off-by: Rama Bondan Prakoso <ramanarubp@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: dotkit <dotkit@electrowizard.me>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -536,6 +536,9 @@ extern struct cpufreq_governor cpufreq_gov_schedutil;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
 extern struct cpufreq_governor cpufreq_gov_chill;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_ACTIVE)
+extern struct cpufreq_governor cpufreq_gov_blu_active;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_active)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)

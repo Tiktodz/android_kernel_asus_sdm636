e3c57b32db9fdc635e096bb8936d9a4ff3243e23
Yi-wei Zhao <gbjc64@motorola.com>
2015-10-19 18:43:19 -0700

    arm: idle: add missing need_resched check
    Add missing need_resched check before calling cpu_do_idle, else
    the cpu will sleep in idle with resched flag is set, and it can
    not be woken up by resched request anymore.
    
    Signed-off-by: Lianwei Wang <a22439@motorola.com>
    Signed-off-by: Yi-wei Zhao <gbjc64@motorola.com>
    Reviewed-on: http://gerrit.mot.com/792800
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>
    Signed-off-by: Joe Maples <joe@frap129.org>
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- arch/arm/kernel/process.c
+++ arch/arm/kernel/process.c
@@ -66,10 +66,12 @@ void (*arm_pm_idle)(void);
 
 void arch_cpu_idle(void)
 {
-	if (arm_pm_idle)
-		arm_pm_idle();
-	else
-		cpu_do_idle();
+	if (!need_resched()) {
+		if (arm_pm_idle)
+			arm_pm_idle();
+		else
+			cpu_do_idle();
+	}
 	local_irq_enable();
 }
 

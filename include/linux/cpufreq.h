001a073df6f87c59397c2825314f179c9a7da33b
Joe Maples <joe@frap129.org>
2016-07-10 17:26:44 +0000

    cpufreq: Chill governor for 4.4
    Signed-off-by: Joe Maples <joe@frap129.org>
    Change-Id: I6558c27ff7bff21b24d8dad60981ec26ae7df268
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -533,6 +533,9 @@ extern struct cpufreq_governor cpufreq_gov_sched;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CHILL)
+extern struct cpufreq_governor cpufreq_gov_chill;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_chill)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)

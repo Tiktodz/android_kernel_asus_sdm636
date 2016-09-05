5a7073c95615ab73ed54d187515c947a8bd0ecce
Will Deacon <will.deacon@arm.com>
2016-09-05 11:56:05 +0100

    arm64: spinlocks: implement smp_mb__before_spinlock() as smp_mb()
    smp_mb__before_spinlock() is intended to upgrade a spin_lock() operation
    to a full barrier, such that prior stores are ordered with respect to
    loads and stores occuring inside the critical section.
    
    Unfortunately, the core code defines the barrier as smp_wmb(), which
    is insufficient to provide the required ordering guarantees when used in
    conjunction with our load-acquire-based spinlock implementation.
    
    This patch overrides the arm64 definition of smp_mb__before_spinlock()
    to map to a full smp_mb().
    
    Cc: <stable@vger.kernel.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Reported-by: Alan Stern <stern@rowland.harvard.edu>
    Signed-off-by: Will Deacon <will.deacon@arm.com>
    Signed-off-by: Catalin Marinas <catalin.marinas@arm.com>
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- arch/arm64/include/asm/spinlock.h
+++ arch/arm64/include/asm/spinlock.h
@@ -143,4 +143,14 @@ static inline int arch_spin_is_contended(arch_spinlock_t *lock)
  */
 #define smp_mb__before_spinlock()	smp_mb()
 
+/*
+ * Accesses appearing in program order before a spin_lock() operation
+ * can be reordered with accesses inside the critical section, by virtue
+ * of arch_spin_lock being constructed using acquire semantics.
+ *
+ * In cases where this is problematic (e.g. try_to_wake_up), an
+ * smp_mb__before_spinlock() can restore the required ordering.
+ */
+#define smp_mb__before_spinlock()	smp_mb()
+
 #endif /* __ASM_SPINLOCK_H */

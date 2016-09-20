54d41be47a27fbdeb97142eef38933fb916f4f64
Neal Cardwell <ncardwell@google.com>
2016-09-19 23:39:17 -0400

    tcp: allow congestion control module to request TSO skb segment count
    Add the tso_segs_goal() function in tcp_congestion_ops to allow the
    congestion control module to specify the number of segments that
    should be in a TSO skb sent by tcp_write_xmit() and
    tcp_xmit_retransmit_queue(). The congestion control module can either
    request a particular number of segments in TSO skb that we transmit,
    or return 0 if it doesn't care.
    
    This allows the upcoming BBR congestion control module to select small
    TSO skb sizes if the module detects that the bottleneck bandwidth is
    very low, or that the connection is policed to a low rate.
    
    Signed-off-by: Van Jacobson <vanj@google.com>
    Signed-off-by: Neal Cardwell <ncardwell@google.com>
    Signed-off-by: Yuchung Cheng <ycheng@google.com>
    Signed-off-by: Nandita Dukkipati <nanditad@google.com>
    Signed-off-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: Soheil Hassas Yeganeh <soheil@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: iamsaalim <saalimquadri1@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>

--- include/net/tcp.h
+++ include/net/tcp.h
@@ -937,7 +937,9 @@ struct tcp_congestion_ops {
 	/* new value of cwnd after loss (optional) */
 	u32  (*undo_cwnd)(struct sock *sk);
 	/* hook for packet ack accounting (optional) */
-	void (*pkts_acked)(struct sock *sk, u32 num_acked, s32 rtt_us);
+	void (*pkts_acked)(struct sock *sk, const struct ack_sample *sample);
+	/* suggest number of segments for each skb to transmit (optional) */
+	u32 (*tso_segs_goal)(struct sock *sk);
 	/* get info for inet_diag (optional) */
 	size_t (*get_info)(struct sock *sk, u32 ext, int *attr,
 			   union tcp_cc_info *info);

007ac6fdc85f46f5390857bca6210711e8214c22
Slawek <yarpiin@gmail.com>
2016-12-10 12:54:03 +0000

    cpufreq: add Lionheart gov
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -568,6 +568,9 @@ extern struct cpufreq_governor cpufreq_gov_nightmare;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BLU_SCHEDUTIL)
 extern struct cpufreq_governor cpufreq_gov_blu_schedutil;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_blu_schedutil)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_LIONHEART)
+extern struct cpufreq_governor cpufreq_gov_lionheart;
+#define CPUFREQ_DEFAULT_GOVERNOR 	(&cpufreq_gov_LIONHEART)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)

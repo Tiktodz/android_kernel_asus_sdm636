c07b66718b139c387f985fbfbcd45a5b132db63b
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2017-04-26 23:22:09 +0200

    PM / wakeup: Integrate mechanism to abort transitions in progress
    The system wakeup framework is not very consistent with respect to
    the way it handles suspend-to-idle and generally wakeup events
    occurring during transitions to system low-power states.
    
    First off, system transitions in progress are aborted by the event
    reporting helpers like pm_wakeup_event() only if the wakeup_count
    sysfs attribute is in use (as documented), but there are cases in
    which system-wide transitions should be aborted even if that is
    not the case.  For example, a wakeup signal from a designated
    wakeup device during system-wide PM transition, it should cause
    the transition to be aborted right away.
    
    Moreover, there is a freeze_wake() call in wakeup_source_activate(),
    but that really is only effective after suspend_freeze_state has
    been set to FREEZE_STATE_ENTER by freeze_enter().  However, it
    is very unlikely that wakeup_source_activate() will ever be called
    at that time, as it could only be triggered by a IRQF_NO_SUSPEND
    interrupt handler, so wakeups from suspend-to-idle don't really
    occur in wakeup_source_activate().
    
    At the same time there is a way to abort a system suspend in
    progress (or wake up the system from suspend-to-idle), which is by
    calling pm_system_wakeup(), but in turn that doesn't cause any
    wakeup source objects to be activated, so it will not be covered
    by wakeup source statistics and will not prevent the system from
    suspending again immediately (in case autosleep is used, for
    example).  Consequently, if anyone wants to abort system transitions
    in progress and allow the wakeup_count mechanism to work, they need
    to use both pm_system_wakeup() and pm_wakeup_event(), say, at the
    same time which is awkward.
    
    For the above reasons, make it possible to trigger
    pm_system_wakeup() from within wakeup_source_activate() and
    provide a new pm_wakeup_hard_event() helper to do so within the
    wakeup framework.
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Signed-off-by: Dušan Uverić <dusan.uveric9@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/base/power/wakeup.c
+++ drivers/base/power/wakeup.c
@@ -542,12 +542,13 @@ static bool wakeup_source_not_registered(struct wakeup_source *ws)
 /**
  * wakup_source_activate - Mark given wakeup source as active.
  * @ws: Wakeup source to handle.
+ * @hard: If set, abort suspends in progress and wake up from suspend-to-idle.
  *
  * Update the @ws' statistics and, if @ws has just been activated, notify the PM
  * core of the event by incrementing the counter of of wakeup events being
  * processed.
  */
-static void wakeup_source_activate(struct wakeup_source *ws)
+static void wakeup_source_activate(struct wakeup_source *ws, bool hard)
 {
 	unsigned int cec;
 
@@ -555,11 +556,8 @@ static void wakeup_source_activate(struct wakeup_source *ws)
 			"unregistered wakeup source\n"))
 		return;
 
-	/*
-	 * active wakeup source should bring the system
-	 * out of PM_SUSPEND_FREEZE state
-	 */
-	freeze_wake();
+	if (hard)
+		pm_system_wakeup();
 
 	ws->active = true;
 	ws->active_count++;
@@ -625,8 +623,9 @@ static bool check_for_block(struct wakeup_source *ws)
 /**
  * wakeup_source_report_event - Report wakeup event using the given source.
  * @ws: Wakeup source to report the event for.
+ * @hard: If set, abort suspends in progress and wake up from suspend-to-idle.
  */
-static void wakeup_source_report_event(struct wakeup_source *ws)
+static void wakeup_source_report_event(struct wakeup_source *ws, bool hard)
 {
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
 	// AP: check if wakelock is on wakelock blocker list
@@ -638,7 +637,7 @@ static void wakeup_source_report_event(struct wakeup_source *ws)
 			ws->wakeup_count++;
 
 		if (!ws->active)
-			wakeup_source_activate(ws);
+			wakeup_source_activate(ws, hard);
 #ifdef CONFIG_BOEFFLA_WL_BLOCKER
 	}
 #endif
@@ -659,7 +658,7 @@ void __pm_stay_awake(struct wakeup_source *ws)
 
 	spin_lock_irqsave(&ws->lock, flags);
 
-	wakeup_source_report_event(ws);
+	wakeup_source_report_event(ws, false);
 	del_timer(&ws->timer);
 	ws->timer_expires = 0;
 
@@ -826,9 +825,10 @@ static void pm_wakeup_timer_fn(unsigned long data)
 }
 
 /**
- * __pm_wakeup_event - Notify the PM core of a wakeup event.
+ * pm_wakeup_ws_event - Notify the PM core of a wakeup event.
  * @ws: Wakeup source object associated with the event source.
  * @msec: Anticipated event processing time (in milliseconds).
+ * @hard: If set, abort suspends in progress and wake up from suspend-to-idle.
  *
  * Notify the PM core of a wakeup event whose source is @ws that will take
  * approximately @msec milliseconds to be processed by the kernel.  If @ws is
@@ -837,7 +837,7 @@ static void pm_wakeup_timer_fn(unsigned long data)
  *
  * It is safe to call this function from interrupt context.
  */
-void __pm_wakeup_event(struct wakeup_source *ws, unsigned int msec)
+void pm_wakeup_ws_event(struct wakeup_source *ws, unsigned int msec, bool hard)
 {
 	unsigned long flags;
 	unsigned long expires;
@@ -847,7 +847,7 @@ void __pm_wakeup_event(struct wakeup_source *ws, unsigned int msec)
 
 	spin_lock_irqsave(&ws->lock, flags);
 
-	wakeup_source_report_event(ws);
+	wakeup_source_report_event(ws, hard);
 
 	if (!msec) {
 		wakeup_source_deactivate(ws);
@@ -866,17 +866,17 @@ void __pm_wakeup_event(struct wakeup_source *ws, unsigned int msec)
  unlock:
 	spin_unlock_irqrestore(&ws->lock, flags);
 }
-EXPORT_SYMBOL_GPL(__pm_wakeup_event);
-
+EXPORT_SYMBOL_GPL(pm_wakeup_ws_event);
 
 /**
  * pm_wakeup_event - Notify the PM core of a wakeup event.
  * @dev: Device the wakeup event is related to.
  * @msec: Anticipated event processing time (in milliseconds).
+ * @hard: If set, abort suspends in progress and wake up from suspend-to-idle.
  *
- * Call __pm_wakeup_event() for the @dev's wakeup source object.
+ * Call pm_wakeup_ws_event() for the @dev's wakeup source object.
  */
-void pm_wakeup_event(struct device *dev, unsigned int msec)
+void pm_wakeup_dev_event(struct device *dev, unsigned int msec, bool hard)
 {
 	unsigned long flags;
 
@@ -884,10 +884,10 @@ void pm_wakeup_event(struct device *dev, unsigned int msec)
 		return;
 
 	spin_lock_irqsave(&dev->power.lock, flags);
-	__pm_wakeup_event(dev->power.wakeup, msec);
+	pm_wakeup_ws_event(dev->power.wakeup, msec, hard);
 	spin_unlock_irqrestore(&dev->power.lock, flags);
 }
-EXPORT_SYMBOL_GPL(pm_wakeup_event);
+EXPORT_SYMBOL_GPL(pm_wakeup_dev_event);
 
 void pm_get_active_wakeup_sources(char *pending_wakeup_source, size_t max)
 {

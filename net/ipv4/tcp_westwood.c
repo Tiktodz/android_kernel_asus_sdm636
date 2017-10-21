a92115df726a0edd2e1a05cfcb59ad4dc1da2a0a
Panchajanya1999 <rsk52959@gmail.com>
2017-10-21 13:57:56 +0530

    Tune Up Westwood TCP a bit
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -43,8 +43,8 @@ struct westwood {
 };
 
 /* TCP Westwood functions and constants */
-#define TCP_WESTWOOD_RTT_MIN   (HZ/18)	/* 55.55ms */
-#define TCP_WESTWOOD_INIT_RTT  (18*HZ)	/* maybe too conservative?! */
+#define TCP_WESTWOOD_RTT_MIN   (HZ/16)	/* 50ms */
+#define TCP_WESTWOOD_INIT_RTT  (16*HZ)	/* maybe too conservative?! */
 
 /*
  * @tcp_westwood_create

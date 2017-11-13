4985d6382528b1c240ea24e9a795dc6a8527c1a2
Harsh Shandilya <msfjarvis@gmail.com>
2017-11-13 17:16:56 +0530

    drivers: cpufreq: Use interruptible waits
    Change-Id: Iea4e2c446c0946a09b9ed5ea787d4560fdc34884
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1234,7 +1234,7 @@ static void cpufreq_policy_put_kobj(struct cpufreq_policy *policy, bool notify)
 	 * proceed with unloading.
 	 */
 	pr_debug("waiting for dropping of refcount\n");
-	wait_for_completion(cmp);
+	wait_for_completion_interruptible(cmp);
 	pr_debug("wait complete\n");
 }
 

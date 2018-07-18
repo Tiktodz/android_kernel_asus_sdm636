72be1de563df3ccc4189d869cab6384cc4f77a32
Ashay Jaiswal <ashayj@codeaurora.org>
2018-07-18 14:38:51 +0530

    power: smb-lib: Fix mutex deadlock during PD hard reset
    rdstd_cc2_detach_work relies on "pd_hard_reset" flag for holding
    "chg_lock" mutex, as pd_hard_reset is updated asynchronously there
    could be a situation where "pd_hard_reset" is updated while
    rdstd_cc2_detach_work is running thus resulting in deadlock.
    Fix this by maintaining locking status in a variable and avoid lock
    acquisition if lock is already held.
    
    Change-Id: Iff9ee211ef4b80c5d48b3d73f0ccbe010434fd6c
    Signed-off-by: Ashay Jaiswal <ashayj@codeaurora.org>
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -5608,7 +5608,6 @@ static void rdstd_cc2_detach_work(struct work_struct *work)
 {
 	int rc;
 	u8 stat4, stat5;
-	bool lock = false;
 	struct smb_charger *chg = container_of(work, struct smb_charger,
 						rdstd_cc2_detach_work);
 
@@ -5677,22 +5676,15 @@ static void rdstd_cc2_detach_work(struct work_struct *work)
 	 * during pd_hard_reset from the function smblib_cc2_sink_removal_exit
 	 * which is called in the same lock context that we try to acquire in
 	 * this work routine.
-	 * Check if this work is running during pd_hard_reset and use trylock
-	 * instead of mutex_lock to prevent any deadlock if mutext is already
-	 * held.
+	 * Check if this work is running during pd_hard_reset and skip holding
+	 * mutex if lock is already held.
 	 */
-	if (chg->pd_hard_reset) {
-		if (mutex_trylock(&chg->lock))
-			lock = true;
-	} else {
+	if (!chg->in_chg_lock)
 		mutex_lock(&chg->lock);
-		lock = true;
-	}
-
 	smblib_usb_typec_change(chg);
-
-	if (lock)
+	if (!chg->in_chg_lock)
 		mutex_unlock(&chg->lock);
+
 	return;
 
 rerun:

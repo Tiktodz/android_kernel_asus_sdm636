28863a4eb0a29853e24c558380fb6e524a17ee15
celtare21 <celtare21@gmail.com>
2018-10-07 13:04:21 +0000

    treewide: Kthread worker API cleanup
    __init_kthread_worker()		-> __kthread_init_worker()
    init_kthread_worker()		-> kthread_init_worker()
    init_kthread_work()		-> kthread_init_work()
    insert_kthread_work()		-> kthread_insert_work()
    queue_kthread_work()		-> kthread_queue_work()
    flush_kthread_work()		-> kthread_flush_work()
    flush_kthread_worker()		-> kthread_flush_worker()
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- drivers/gpu/msm/kgsl.c
+++ drivers/gpu/msm/kgsl.c
@@ -5098,7 +5098,7 @@ static int __init kgsl_core_init(void)
 	kgsl_driver.mem_workqueue = alloc_workqueue("kgsl-mementry",
 		WQ_UNBOUND | WQ_MEM_RECLAIM, 0);
 
-	init_kthread_worker(&kgsl_driver.worker);
+	kthread_init_worker(&kgsl_driver.worker);
 
 	kgsl_driver.worker_thread = kthread_run(kthread_worker_fn,
 		&kgsl_driver.worker, "kgsl_worker_thread");

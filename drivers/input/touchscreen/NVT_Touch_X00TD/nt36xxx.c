cabba45e1c33188b9050cd4fe2585106b2079c0a
SagarMakhar <sagarmakhar@gmail.com>
2018-10-19 01:27:33 +0000

    X00TD: Nvt-ts: enable some more gestures and change name to zen
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>
    Change-Id: Ia3ccc635832d92a28a63435cb622b3b5486e9fd6
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.c
+++ drivers/input/touchscreen/NVT_Touch_X00TD/nt36xxx.c
@@ -394,9 +394,13 @@ const uint16_t touch_key_array[TOUCH_KEY_NUM] = {
 #define GESTURE_EVENT_V 		252
 #define GESTURE_EVENT_W 		253
 #define GESTURE_EVENT_Z 		254
+#define GESTURE_EVENT_SWIPE_UP          255
+#define GESTURE_EVENT_SWIPE_DOWN        256
+#define GESTURE_EVENT_SWIPE_LEFT        257
+#define GESTURE_EVENT_SWIPE_RIGHT       258
 /* Huaqin modify  for TT1176710 by liunianliang at 2018/03/30 end */
 /* Huaqin modify gesture keycode by yuexinghan 20171109 start */
-#define GESTURE_EVENT_SWIPE_UP 248
+/*#define GESTURE_EVENT_SWIPE_UP 248*/
 #define GESTURE_EVENT_DOUBLE_CLICK	KEY_WAKEUP
 /* Huaqin modify gesture keycode by yuexinghan 20171109 end */
 
@@ -411,9 +415,9 @@ const uint16_t gesture_key_array[] = {
 	GESTURE_EVENT_E,
 	GESTURE_EVENT_S,
 	GESTURE_EVENT_SWIPE_UP,
-	KEY_WAKEUP,
-	KEY_WAKEUP,
-	KEY_WAKEUP,
+	GESTURE_EVENT_SWIPE_DOWN,
+	GESTURE_EVENT_SWIPE_LEFT,
+	GESTURE_EVENT_SWIPE_RIGHT,
 };
 /* Huaqin add by yuexinghan for gesture mode 20171030 end */
 #endif
@@ -1111,9 +1115,9 @@ int nvt_test_node_init(struct platform_device *tpinfo_device)
 #define ID_GESTURE_WORD_e			19
 #define ID_GESTURE_WORD_S			20
 #define ID_GESTURE_SLIDE_UP		21
-//#define GESTURE_SLIDE_DOWN		22
-//#define GESTURE_SLIDE_LEFT		23
-//#define GESTURE_SLIDE_RIGHT		24
+#define GESTURE_SLIDE_DOWN		22
+#define GESTURE_SLIDE_LEFT		23
+#define GESTURE_SLIDE_RIGHT		24
 
 static struct wake_lock gestrue_wakelock;
 
@@ -1198,7 +1202,7 @@ void nvt_ts_wakeup_gesture_report(uint8_t gesture_id)
 				keycode = gesture_key_array[9];
 			}
 			break;
-		/* case GESTURE_SLIDE_DOWN:
+		case GESTURE_SLIDE_DOWN:
 			NVT_LOG("Gesture : Slide DOWN.\n");
 			keycode = gesture_key_array[10];
 			break;
@@ -1209,7 +1213,7 @@ void nvt_ts_wakeup_gesture_report(uint8_t gesture_id)
 		case GESTURE_SLIDE_RIGHT:
 			NVT_LOG("Gesture : Slide RIGHT.\n");
 			keycode = gesture_key_array[12];
-			break; */
+			break;
 /* Huaqin add by yuexinghan for gesture mode 20171030 end */
 		default:
 			NVT_LOG("Still in gesture mode.\n");

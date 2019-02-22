a84479fee25adcd24e4495ba4219992754edd2a9
Pablo Mendez Hernandez <pablomh@gmail.com>
2019-02-22 23:14:17 +0530

    input: misc: gf_spi: Convert to wakeup sources suspend blocker
    On kernel 4.9 the Android wakelocks are finally gone.
    Convert the driver to use the wakeup sources in the Linux PM
    API to achieve the very same goal.
    
    Signed-off-by: Pablo Mendez Hernandez <pablomh@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>
    Change-Id: I063ee93b7d28680db8526ec249667cf519ca8fea

--- drivers/input/fingerprint/goodixfp/gf_spi.c
+++ drivers/input/fingerprint/goodixfp/gf_spi.c
@@ -40,7 +40,7 @@
 #include <linux/fb.h>
 #include <linux/pm_qos.h>
 #include <linux/cpufreq.h>
-#include <linux/wakelock.h>
+// #include <linux/wakelock.h>
 #include "gf_spi.h"
 
 #include "../common/fingerprint_common.h"
@@ -72,7 +72,7 @@ static int SPIDEV_MAJOR;
 static DECLARE_BITMAP(minors, N_SPI_MINORS);
 static LIST_HEAD(device_list);
 static DEFINE_MUTEX(device_list_lock);
-static struct wake_lock fp_wakelock;
+static struct wakeup_source fp_wakelock;
 static struct gf_dev gf;
 
 struct gf_key_map maps[] = {
@@ -493,7 +493,7 @@ static irqreturn_t gf_irq(int irq, void *handle)
 {
 #if defined(GF_NETLINK_ENABLE)
 	char temp = GF_NET_EVENT_IRQ;
-	wake_lock_timeout(&fp_wakelock, msecs_to_jiffies(WAKELOCK_HOLD_TIME));
+	__pm_wakeup_event(&fp_wakelock, WAKELOCK_HOLD_TIME);
 	sendnlmsg(&temp);
 #elif defined (GF_FASYNC)
 	struct gf_dev *gf_dev = &gf;
@@ -752,7 +752,7 @@ static int gf_probe(struct platform_device *pdev)
 #ifndef USE_COMMON_FP
 	gf_dev->irq = gf_irq_num(gf_dev);
 #endif
-	wake_lock_init(&fp_wakelock, WAKE_LOCK_SUSPEND, "fp_wakelock");
+	wakeup_source_init(&fp_wakelock, "fp_wakelock");
 
 
 	pr_info("version V%d.%d.%02d\n", VER_MAJOR, VER_MINOR, PATCH_LEVEL);
@@ -792,7 +792,7 @@ static int gf_remove(struct platform_device *pdev)
 {
 	struct gf_dev *gf_dev = &gf;
 
-	wake_lock_destroy(&fp_wakelock);
+	wakeup_source_trash(&fp_wakelock);
 	/* make sure ops on existing fds can abort cleanly */
 	if (gf_dev->irq)
 		free_irq(gf_dev->irq, gf_dev);

3afb2ee3460af46653e8b26844abbf0b0340f719
Sultan Alsawaf <sultan@kerneltoast.com>
2019-04-14 20:47:49 -0700

    treewide: Fix code issues detected using GCC 9
    Use the latest version of GCC to take advantage of improved static
    analysis. These issues appeared as warnings from the compiler.
    
    Many of these fixes are for clearly incorrect code; compiler warnings
    should not be taken lightly.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I1b4dc48734a6c3379a28a5962ad0193c46475785
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- drivers/misc/qcom/qdsp6v2/audio_amrwbplus.c
+++ drivers/misc/qcom/qdsp6v2/audio_amrwbplus.c
@@ -53,7 +53,7 @@ static long audio_ioctl_shared(struct file *file, unsigned int cmd,
 	struct q6audio_aio *audio = file->private_data;
 	int rc = 0;
 
-	switch (cmd) {
+	switch (cmd)
 	case AUDIO_START: {
 		pr_err("%s[%pK]: AUDIO_START session_id[%d]\n", __func__,
 			audio, audio->ac->session);
@@ -104,10 +104,8 @@ static long audio_ioctl_shared(struct file *file, unsigned int cmd,
 		pr_debug("%s:AUDIO_START sessionid[%d]enable[%d]\n", __func__,
 			audio->ac->session,
 			audio->enabled);
-		if (audio->stopped == 1)
-			audio->stopped = 0;
-			break;
-		}
+		audio->stopped = 0;
+		break;
 	default:
 		pr_err("%s: Unknown ioctl cmd = %d", __func__, cmd);
 		rc = -EINVAL;

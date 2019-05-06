ec84e81ba50bf6a521d99d5f6cf07f1bd4c1fa23
Tyler Nijmeh <tylernij@gmail.com>
2019-05-06 14:31:11 -0700

    power: qpnp-fg: Don't hold wakelocks
    Fuelgauge is relatively unimportant in the wide spectrum of kernel
    drivers. Fuelgauge uses far too many wakelocks than are necessary.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: ironbuang <thirdzcee@github.com>
    Signed-off-by: clarencelol <clarencekuiek@icloud.com>

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -2652,52 +2652,6 @@ out:
 	return rc;
 }
 
-#define SANITY_CHECK_PERIOD_MS	5000
-static void check_sanity_work(struct work_struct *work)
-{
-	struct fg_chip *chip = container_of(work,
-				struct fg_chip,
-				check_sanity_work.work);
-	int rc = 0;
-	u8 beat_count;
-	bool tried_once = false;
-
-	fg_stay_awake(&chip->sanity_wakeup_source);
-
-try_again:
-	rc = fg_read(chip, &beat_count,
-			chip->mem_base + MEM_INTF_FG_BEAT_COUNT, 1);
-	if (rc) {
-		pr_err("failed to read beat count rc=%d\n", rc);
-		goto resched;
-	}
-
-	if (fg_debug_mask & FG_STATUS)
-		pr_info("current: %d, prev: %d\n", beat_count,
-			chip->last_beat_count);
-
-	if (chip->last_beat_count == beat_count) {
-		if (!tried_once) {
-			/* Wait for 1 FG cycle and read it once again */
-			msleep(1500);
-			tried_once = true;
-			goto try_again;
-		} else {
-			pr_err("Beat count not updating\n");
-			fg_check_ima_error_handling(chip);
-			goto out;
-		}
-	} else {
-		chip->last_beat_count = beat_count;
-	}
-resched:
-	schedule_delayed_work(
-		&chip->check_sanity_work,
-		msecs_to_jiffies(SANITY_CHECK_PERIOD_MS));
-out:
-	fg_relax(&chip->sanity_wakeup_source);
-}
-
 #define SRAM_TIMEOUT_MS			3000
 static void update_sram_data_work(struct work_struct *work)
 {

84ebd265ff36d2d34a5165b4f80ec5ef6c28aae0
pranavkapoor001 <pranavkapoor001@gmail.com>
2019-08-03 12:58:07 +0530

    Update Boeffla WL List & Enable
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- drivers/base/power/boeffla_wl_blocker.h
+++ drivers/base/power/boeffla_wl_blocker.h
@@ -15,9 +15,8 @@
  */
 
 #define BOEFFLA_WL_BLOCKER_VERSION	"1.1.0"
-
-#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK"
+#define LIST_WL_DEFAULT				"qcom_rx_wakelock;wlan;wlan_wow_wl;wlan_extscan_wl;netmgr_wl;NETLINK;IPA_WS;[timerfd];wlan_ipa;wlan_pno_wl;wcnss_filter_lock;wlan_rx_wake;wlan_ctrl_wake;wlan_wake;qbt_wake_source;sensor_ind;radio-interface;qcril;IPCRTR_lpass_rx"
 
 #define LENGTH_LIST_WL				255
-#define LENGTH_LIST_WL_DEFAULT		100
+#define LENGTH_LIST_WL_DEFAULT		226
 #define LENGTH_LIST_WL_SEARCH		LENGTH_LIST_WL + LENGTH_LIST_WL_DEFAULT + 5

58455baaed17a73645b69030e79c0b33709957ef
anomalchik <vidslava@yandex.ru>
2019-09-17 11:48:38 +0000

    X00T: drivers: input: add fingerprints
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -601,6 +601,14 @@
 #define KEY_SCREENSAVER		0x245	/* AL Screen Saver */
 #define KEY_VOICECOMMAND		0x246	/* Listening Voice Command */
 
+#define FP_KEY_UP			0x258
+#define FP_KEY_DOWN		0x259
+#define FP_KEY_LEFT		0x25a
+#define FP_KEY_RIGHT		0x25b
+#define FP_KEY_CLICK		0x25c
+#define FP_KEY_DOUBLE_CLICK		0x25d
+#define FP_KEY_LONG_PRESS          0x25e
+
 #define KEY_BRIGHTNESS_MIN		0x250	/* Set Brightness to Minimum */
 #define KEY_BRIGHTNESS_MAX		0x251	/* Set Brightness to Maximum */
 

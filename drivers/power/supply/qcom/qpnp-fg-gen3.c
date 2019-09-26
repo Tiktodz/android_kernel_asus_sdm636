24965fb4be12f6c6d708bd22c14f3eb2dac935e3
ajaivasudeve <ajaivasudeve@gmail.com>
2019-09-26 23:18:39 +0530

    power: hack fg_get_battery_temp to report reading from pa_therm0 as battery temperature
    Some X00T devices seems to be having a faulty temperature sensor for the
    battery, reporting temperatures with upto 8 DegC error at times. This leads
    to unnecessary charging throttle, As a workaround for this, hack fg_get_battery_temp
    function to report temperature from pa_therm0 as the battery temperature.
    Report it with an offset of -2 DegC because pa_therm0[VADC_AMUX_THM3_PU2 | 0x4f]
    usually stays at about 2 DegC higher than the battery temperature.
    
    Signed-off-by: ajaivasudeve <ajaivasudeve@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: Tiktodz <ewprjkt@proton.me>
    Signed-off-by: Kneba <abenkenary3@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/power/supply/qcom/qpnp-fg-gen3.c
+++ drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -22,6 +22,7 @@
 #include <linux/qpnp/qpnp-revid.h>
 #include "fg-core.h"
 #include "fg-reg.h"
+#include <linux/qpnp/qpnp-adc.h>
 
 #define FG_GEN3_DEV_NAME	"qcom,fg-gen3"
 
@@ -636,24 +637,33 @@ static int fg_get_jeita_threshold(struct fg_chip *chip,
 #define BATT_TEMP_DENR		1
 static int fg_get_battery_temp(struct fg_chip *chip, int *val)
 {
-	int rc = 0, temp;
-	u8 buf[2];
+	int rc = 0;
+	int64_t temp;
+	struct qpnp_vadc_result result;
 
-	rc = fg_read(chip, BATT_INFO_BATT_TEMP_LSB(chip), buf, 2);
-	if (rc < 0) {
-		pr_err("failed to read addr=0x%04x, rc=%d\n",
-			BATT_INFO_BATT_TEMP_LSB(chip), rc);
+	chip->vadc_dev = qpnp_get_vadc(chip->dev, "vadc_therm");
+
+	if (IS_ERR(chip->vadc_dev)) {
+		rc = PTR_ERR(chip->vadc_dev);
+		if (rc != -EPROBE_DEFER)
+			pr_err("VADC property: vadc_therm missing\n");
 		return rc;
 	}
 
-	temp = ((buf[1] & BATT_TEMP_MSB_MASK) << 8) |
-		(buf[0] & BATT_TEMP_LSB_MASK);
-	temp = DIV_ROUND_CLOSEST(temp, 4);
 
-	/* Value is in Kelvin; Convert it to deciDegC */
-	temp = (temp - 273) * 10;
-	*val = temp;
-	return 0;
+	rc = qpnp_vadc_read(chip->vadc_dev, VADC_AMUX_THM3_PU2, &result);
+	if (!rc) {
+		temp = result.physical;
+		/* Value is in DegreeC; Convert it to deciDegC.
+		   Also, add an offset of -20 deciDegC, to report
+		   a more probable battery temperature. */
+		temp = (temp * 10) - 20;
+		*val = temp;
+		return rc;
+	} else {
+		pr_err("Unable to read battery_temp\n");
+		return rc;
+	}
 }
 
 static int fg_get_battery_resistance(struct fg_chip *chip, int *val)

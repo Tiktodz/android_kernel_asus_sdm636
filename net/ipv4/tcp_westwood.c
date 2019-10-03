322561de47051c01d0d5e32db1109624f870514d
DarknessKiller <kaelvinchen2@gmail.com>
2019-10-03 15:24:50 +0800

    westwood: Custom Values
    Signed-off-by: Tiktodz <ewprjkt@proton.me>

--- net/ipv4/tcp_westwood.c
+++ net/ipv4/tcp_westwood.c
@@ -43,8 +43,8 @@ struct westwood {
 };
 
 /* TCP Westwood functions and constants */
-#define TCP_WESTWOOD_RTT_MIN   (HZ/20)	/* 50ms */
-#define TCP_WESTWOOD_INIT_RTT  (20*HZ)	/* maybe too conservative?! */
+#define TCP_WESTWOOD_RTT_MIN   (HZ/16)	/* 50ms */
+#define TCP_WESTWOOD_INIT_RTT  (16*HZ)	/* maybe too conservative?! */
 
 /*
  * @tcp_westwood_create

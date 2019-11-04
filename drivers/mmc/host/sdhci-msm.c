52e5e17f29e9405db9cd427a3673557f2dab8526
ktommy91 <kemenesi.tamas@gmail.com>
2019-11-04 21:56:27 +0100

    mmc: disable tracing
    Change-Id: I3ef89726ed3c5425223f94f70db93cfc3194e563
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -5096,8 +5096,8 @@ defer_disable_host_irq:
 			pr_err("%s: failed to suspend crypto engine %d\n",
 					mmc_hostname(host->mmc), ret);
 	}
-	trace_sdhci_msm_runtime_suspend(mmc_hostname(host->mmc), 0,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+	//trace_sdhci_msm_runtime_suspend(mmc_hostname(host->mmc), 0,
+	//		ktime_to_us(ktime_sub(ktime_get(), start)));
 	return 0;
 }
 
@@ -5130,8 +5130,8 @@ skip_ice_resume:
 defer_enable_host_irq:
 	enable_irq(msm_host->pwr_irq);
 
-	trace_sdhci_msm_runtime_resume(mmc_hostname(host->mmc), 0,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+	//trace_sdhci_msm_runtime_resume(mmc_hostname(host->mmc), 0,
+	//		ktime_to_us(ktime_sub(ktime_get(), start)));
 	return 0;
 }
 
@@ -5165,8 +5165,8 @@ out:
 			sdhci_cfg_irq(host, false, true);
 	}
 
-	trace_sdhci_msm_suspend(mmc_hostname(host->mmc), ret,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+	//trace_sdhci_msm_suspend(mmc_hostname(host->mmc), ret,
+	//		ktime_to_us(ktime_sub(ktime_get(), start)));
 	return ret;
 }
 
@@ -5197,8 +5197,8 @@ out:
 			sdhci_cfg_irq(host, true, true);
 	}
 
-	trace_sdhci_msm_resume(mmc_hostname(host->mmc), ret,
-			ktime_to_us(ktime_sub(ktime_get(), start)));
+	//trace_sdhci_msm_resume(mmc_hostname(host->mmc), ret,
+	//		ktime_to_us(ktime_sub(ktime_get(), start)));
 	return ret;
 }
 

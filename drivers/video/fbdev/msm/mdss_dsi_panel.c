41aff43b873d92718f614dec9adba47c10f179dd
Dede Dindin Qudsy <xtrymind@gmail.com>
2020-01-12 11:01:00 +0700

    msm: mdss: dsi: set MIN_REFRESH_RATE
    this is based on caf 4.14 which set min_refresh_rate to 48.
    
    Signed-off-by: Dede Dindin Qudsy <xtrymind@gmail.com>

--- drivers/video/fbdev/msm/mdss_dsi_panel.c
+++ drivers/video/fbdev/msm/mdss_dsi_panel.c
@@ -31,6 +31,7 @@
 #endif
 
 #define DT_CMD_HDR 6
+#define MIN_REFRESH_RATE 48
 #define DEFAULT_MDP_TRANSFER_TIME 14000
 
 #define VSYNC_DELAY msecs_to_jiffies(17)
@@ -2313,7 +2314,7 @@ static int mdss_dsi_set_refresh_rate_range(struct device_node *pan_node,
 		 * If min refresh rate is not specified, set it to the
 		 * default panel refresh rate.
 		 */
-		pinfo->min_fps = pinfo->mipi.frame_rate;
+		pinfo->min_fps = MIN_REFRESH_RATE;
 		rc = 0;
 	}
 

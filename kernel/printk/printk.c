1ddea56be89fdb83c623ae1acddbe63e4c2c507f
Julian Liu <wlootlxt123@gmail.com>
2020-03-03 23:58:58 +0800

    kernel: printk: make kmsg as no-op
    Signed-off-by: Julian Liu <wlootlxt123@gmail.com>
    Change-Id: I0e26333c5ebb7f83f378d1a40e986dae24571ac0

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -618,6 +618,7 @@ struct devkmsg_user {
 
 static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 {
+#if 0
 	char *buf, *line;
 	int level = default_message_loglevel;
 	int facility = 1;	/* LOG_USER */
@@ -671,6 +672,9 @@ static ssize_t devkmsg_write(struct kiocb *iocb, struct iov_iter *from)
 ignore:
 	kfree(buf);
 	return ret;
+#else
+	return iov_iter_count(from);
+#endif
 }
 
 static ssize_t devkmsg_read(struct file *file, char __user *buf,

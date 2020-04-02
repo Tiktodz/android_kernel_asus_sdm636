8fb2fa55f629743e8d92538891793d91ef70b7fb
Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>
2020-04-02 16:35:51 +0000

    power: smb-lib: Use 2.1A for DCP charging
    Signed-off-by: Yasir-Siddiqui <www.mohammad.yasir.s@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2697,7 +2697,7 @@ int smblib_get_prop_die_health(struct smb_charger *chg,
 #ifdef CONFIG_MACH_ASUS_SDM660
 #define DCP_CURRENT_UA			2000000
 #else
-#define DCP_CURRENT_UA			1500000
+#define DCP_CURRENT_UA			2100000
 #endif
 #define HVDCP_CURRENT_UA		3000000
 #define TYPEC_DEFAULT_CURRENT_UA	900000

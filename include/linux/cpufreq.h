81d93bb723e0ef95804b0b7576883dc2034b1ba5
Sonal Singh <sonal.singh.19993@gmail.com>
2020-04-05 12:07:27 +0000

    cpufrq: Add intelliactive & interactivex governors
    Signed-off-by: Sonal Singh <sonal.singh.19993@gmail.com>
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: dotkit <dotkit@electrowizard.me>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -542,6 +542,12 @@ extern struct cpufreq_governor cpufreq_gov_blu_active;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_NEBULA)
 extern struct cpufreq_governor cpufreq_gov_nebula;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_nebula)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIACTIVE)
+extern struct cpufreq_governor cpufreq_gov_intelliactive;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_intelliactive)
 #endif
 
 static inline void cpufreq_policy_apply_limits(struct cpufreq_policy *policy)

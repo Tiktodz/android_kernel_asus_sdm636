c759ca0c29c7d023b0acc3cf323034c567e1a3fb
Rizky Benggolo <travarilo@gmail.com>
2020-04-26 11:55:32 +0700

    lmk: Set default vmpressure_file_min value
    Signed-off-by: Rizky Benggolo <travarilo@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -251,7 +251,7 @@ module_param_named(enable_adaptive_lmk, enable_adaptive_lmk, int,
  * 90-94. Usually this is a pseudo minfree value, higher than the
  * highest configured value in minfree array.
  */
-static int vmpressure_file_min;
+static int vmpressure_file_min = 89600;
 module_param_named(vmpressure_file_min, vmpressure_file_min, int,
 		   S_IRUGO | S_IWUSR);
 

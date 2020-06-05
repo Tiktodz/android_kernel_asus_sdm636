50977fa5b105efdd7ecf87035a8d1407335989cd
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-06-05 20:49:23 +0530

    X01BD: drivers: input: Import nt36672 and hx83112a touchscreen drivers
    * from Q X01BD 424 kernel source
    
    Change-Id: I814f5ad94b8c51c5db2766a35142ce0082a80cde
    Signed-off-by: Tiktodz <ewprjkt@proton.me>
    Signed-off-by: Kneba <abenkenary3@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- include/uapi/linux/input-event-codes.h
+++ include/uapi/linux/input-event-codes.h
@@ -663,6 +663,17 @@
 #define BTN_TRIGGER_HAPPY39		0x2e6
 #define BTN_TRIGGER_HAPPY40		0x2e7
 
+#ifdef CONFIG_MACH_ASUS_SDM660
+#define KEY_TP_GESTURE_SWIPE_UP		0x2F6
+#define KEY_TP_GESTURE_DOUBLE_CLICK	0x2F7
+#define KEY_TP_GESTURE_C		0x2F0
+#define KEY_TP_GESTURE_E		0x2ED
+#define KEY_TP_GESTURE_S		0x2EE
+#define KEY_TP_GESTURE_V		0x2F1
+#define KEY_TP_GESTURE_W		0x2EC
+#define KEY_TP_GESTURE_Z		0x2EF
+#endif
+
 /* We avoid low common keys in module aliases so they don't get huge. */
 #define KEY_MIN_INTERESTING	KEY_MUTE
 #define KEY_MAX			0x2ff

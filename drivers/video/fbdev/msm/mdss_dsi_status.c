703ee8960a7fbfc2abbecc8b007379c91cda51ec
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-06-05 22:21:49 +0530

    msm: mdss: Import ASUS changes
    * from Q X01BD 424 kernel source
    
    Change-Id: Iaf701bae8050c1c24c108b987770d9cb587a6ba4

--- drivers/video/fbdev/msm/mdss_dsi_status.c
+++ drivers/video/fbdev/msm/mdss_dsi_status.c
@@ -30,7 +30,11 @@
 #include "mdss_panel.h"
 #include "mdss_mdp.h"
 
+#ifdef CONFIG_MACH_ASUS_X00TD
+#define STATUS_CHECK_INTERVAL_MS 2000
+#else
 #define STATUS_CHECK_INTERVAL_MS 5000
+#endif
 #define STATUS_CHECK_INTERVAL_MIN_MS 50
 #define DSI_STATUS_CHECK_INIT -1
 #define DSI_STATUS_CHECK_DISABLE 1
@@ -258,6 +262,9 @@ int __init mdss_dsi_status_init(void)
 		return -ENOMEM;
 	}
 
+#ifdef CONFIG_MACH_ASUS_SDM660
+	pstatus_data->is_first_check = 1;
+#endif
 	pstatus_data->fb_notifier.notifier_call = fb_event_callback;
 
 	rc = fb_register_client(&pstatus_data->fb_notifier);
--- drivers/video/fbdev/msm/mdss_fb.h
+++ drivers/video/fbdev/msm/mdss_fb.h
@@ -287,6 +287,10 @@ struct msm_fb_data_type {
 	struct msm_fb_fps_info fps_info;
 	struct delayed_work idle_notify_work;
 
+#ifdef CONFIG_MACH_ASUS_X01BD
+	struct delayed_work early_unblank_work;
+	bool early_unblank_work_queued;
+#endif
 	bool atomic_commit_pending;
 
 	int op_enable;

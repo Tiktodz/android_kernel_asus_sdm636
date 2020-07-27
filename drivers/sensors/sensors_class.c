46be5b969dca4b1ccaff807ef2fcead24394a0e4
Saurabh Charde <saurabhchardereal@gmail.com>
2020-07-27 17:04:16 +0530

    drivers: sensors: Silence more STK3321 sensor log spam
    [11941.832634] stk3x1x_enable_ps: ps input event=1, ps=85
    [15920.820204] stk3x1x_suspend
    [15920.980633] stk3x1x_resume
    
    * Also silence this error:
    [11941.832817] sensors stk3x1x-proximity: Invalid sensor class delay handle
    
    Signed-off-by: Saurabh Charde <saurabhchardereal@gmail.com>
    Signed-off-by: wimbiyoas <wyoashizkia@gmail.com>
    Signed-off-by: fajar4561 <maulanafajar751@gmail.com>

--- drivers/sensors/sensors_class.c
+++ drivers/sensors/sensors_class.c
@@ -237,7 +237,7 @@ static ssize_t sensors_delay_store(struct device *dev,
 		return -EINVAL;
 	}
 	if (sensors_cdev->sensors_poll_delay == NULL) {
-		dev_err(dev, "Invalid sensor class delay handle\n");
+		dev_dbg(dev, "Invalid sensor class delay handle\n");
 		return -EINVAL;
 	}
 	ret = sensors_cdev->sensors_poll_delay(sensors_cdev, data);

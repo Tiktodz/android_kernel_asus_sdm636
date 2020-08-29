81ab31335049ccfcf78347a507beff35aa9c799c
UtsavBalar1231 <utsavbalar1231@gmail.com>
2020-08-29 13:37:45 +0800

    msm_vidc: Remove pm_qos usage
    Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>
    Signed-off-by: Ratoriku <a1063021545@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/media/platform/msm/vidc/venus_hfi.h
+++ drivers/media/platform/msm/vidc/venus_hfi.h
@@ -17,7 +17,6 @@
 #include <linux/clk.h>
 #include <linux/mutex.h>
 #include <linux/platform_device.h>
-#include <linux/pm_qos.h>
 #include <linux/spinlock.h>
 #include "vmem/vmem.h"
 #include "vidc_hfi_api.h"
@@ -255,7 +254,6 @@ struct venus_hfi_device {
 	enum hfi_packetization_type packetization_type;
 	struct msm_vidc_cb_info *response_pkt;
 	u8 *raw_packet;
-	struct pm_qos_request qos;
 	unsigned int skip_pc_count;
 	struct msm_vidc_capability *sys_init_capabilities;
 };

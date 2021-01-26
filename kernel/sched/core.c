72f9dfb99a46ff97a2cc5610ac325e9e842b84d7
Sultan Alsawaf <sultan@kerneltoast.com>
2021-01-25 23:17:11 -0800

    kernel: Extend the perf-critical API to little CPUs
    It's helpful to be able to affine low-priority kthreads to the little
    CPU, such as for deferred memory cleanup. Extend the perf-critical API
    to make this possible.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1249,9 +1249,10 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	unsigned int dest_cpu;
 	int ret = 0;
 
-	/* Force all performance-critical kthreads onto the big cluster */
-	if (p->flags & PF_PERF_CRITICAL)
-		new_mask = cpu_perf_mask;
+	/* Don't allow perf-critical threads to have non-perf affinities */
+	if ((p->flags & PF_PERF_CRITICAL) && new_mask != cpu_lp_mask &&
+		new_mask != cpu_perf_mask)
+		return -EINVAL;
 
 	rq = task_rq_lock(p, &flags);
 

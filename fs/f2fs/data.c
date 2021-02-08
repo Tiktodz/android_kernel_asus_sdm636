dbc90bfb8c31e8509320f52404f1f67d11e36a70
Divyanshu-Modi <divyan.m05@gmail.com>
2021-02-08 16:20:55 +0530

    F2fs: Solve unused variable warn
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -185,7 +185,7 @@ static bool f2fs_bio_post_read_required(struct bio *bio)
 
 static void f2fs_read_end_io(struct bio *bio)
 {
-	struct page *first_page = bio->bi_io_vec[0].bv_page;
+//	struct page *first_page = bio->bi_io_vec[0].bv_page;
 	struct f2fs_sb_info *sbi = F2FS_P_SB(bio->bi_io_vec->bv_page);
 
 	if (time_to_inject(sbi, FAULT_READ_IO)) {
@@ -1975,7 +1975,7 @@ static int f2fs_read_data_pages(struct file *file,
 			struct list_head *pages, unsigned nr_pages)
 {
 	struct inode *inode = mapping->host;
-	struct page *page = list_last_entry(pages, struct page, lru);
+//	struct page *page = list_last_entry(pages, struct page, lru);
 
 //	trace_f2fs_readpages(inode, page, nr_pages);
 

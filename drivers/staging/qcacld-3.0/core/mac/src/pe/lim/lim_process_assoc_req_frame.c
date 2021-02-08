98d579c804d97c1fdfd75e248e7230e5bc75fcaa
Srinivas Dasari <dasaris@codeaurora.org>
2021-02-08 12:12:58 +0530

    qcacld-3.0: Send assoc reject upon failing to post ASSOC_IND
    Currently, lim silently drops the association if it fails to
    post ASSOC_IND due to some reason(e.g. invalid contents of
    assoc request) and the MLM state is stuck in
    eLIM_MLM_WT_ASSOC_CNF_STATE. Station context is not cleaned up
    till the next association. Gracefully cleanup the association
    in such failure cases.
    
    Change-Id: Iede43a1ddc4ac6ef300af02776b153b58dd70c2c
    CRs-Fixed: 2810235
    CVE-2021-1955
    Signed-off-by: Kevin F. Haggerty <haggertk@lineageos.org>
    Signed-off-by: Tiktodz <ewprjkt@proton.me>
    Signed-off-by: Kneba <abenkenary3@gmail.com>

--- drivers/staging/qcacld-3.0/core/mac/src/pe/lim/lim_process_assoc_req_frame.c
+++ drivers/staging/qcacld-3.0/core/mac/src/pe/lim/lim_process_assoc_req_frame.c
@@ -1,5 +1,4 @@
 /*
- * Copyright (c) 2012-2018 The Linux Foundation. All rights reserved.
  * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for

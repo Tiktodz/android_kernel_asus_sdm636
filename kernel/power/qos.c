c9ed59dd40c4b20640fd374018f40f9a5674e898
Tiktodz <ewprjkt@proton.me>
2022-06-05 15:34:57 +0000

    Revert "kernel: Change pm_qos_set_value_for_cpus to void function"
    This reverts commit 5ecbcafeec30db15a5a12a93eb4c4a281f0d2c2e.

--- kernel/power/qos.c
+++ kernel/power/qos.c
@@ -267,7 +267,7 @@ static const struct file_operations pm_qos_debug_fops = {
 	.release        = single_release,
 };
 
-static inline void pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
+static inline int pm_qos_set_value_for_cpus(struct pm_qos_constraints *c,
 		struct cpumask *cpus)
 {
 	struct pm_qos_request *req = NULL;

9a53fc9c9b23c656bdae9423f528f494eb8631ec
kdrag0n <dragon@khronodragon.com>
2022-10-06 08:42:44 +0700

    kernel: Only boost on app launch if recently touched
    This fixes unnecessary background boosting (especially right after boot)
    of services, content providers, and other background processes launching
    as zygote. Max zygote boost will only occur within 150 ms of the latest
    input event.
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2780,7 +2780,8 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
         if (!ret && !threadgroup &&
             !strcmp(of->kn->parent->name, "top-app") &&
             task_is_zygote(tsk->parent)) {
-            cpu_input_boost_kick_max(256);
+            if (cpu_input_boost_within_input(150))
+                cpu_input_boost_kick_max(256);
         }
 
 	put_task_struct(tsk);
--- kernel/fork.c
+++ kernel/fork.c
@@ -1830,8 +1830,11 @@ long _do_fork(unsigned long clone_flags,
 
 	/* Boost CPU to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(50);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
+		if (cpu_input_boost_within_input(150))
+			cpu_input_boost_kick_max(50);
+		
+		if (df_boost_within_input(150))
+			devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 50);
 	}
 
 	/*

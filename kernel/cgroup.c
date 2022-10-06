f9c3c586cf24fb18b7774876614a0ff13c4cbf4c
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2022-10-06 08:44:48 +0700

    kernel: cgroup: Reduce period of time top-app boost
    Top-app transitions are more so bound CPUs than memory
    bound, so reduce short max period of time the CPU.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- kernel/cgroup.c
+++ kernel/cgroup.c
@@ -2780,7 +2780,7 @@ static ssize_t __cgroup_procs_write(struct kernfs_open_file *of, char *buf,
         if (!ret && !threadgroup &&
             !strcmp(of->kn->parent->name, "top-app") &&
             task_is_zygote(tsk->parent)) {
-            cpu_input_boost_kick_max(500);
+            cpu_input_boost_kick_max(256);
         }
 
 	put_task_struct(tsk);

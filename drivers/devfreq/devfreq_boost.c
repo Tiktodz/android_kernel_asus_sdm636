fa92b9b96d6b89d870a46135c0b230b25c60a78c
Danny Lin <danny@kdrag0n.dev>
2022-10-06 08:29:38 +0700

    devfreq: devfreq_boost: Track input event times and expose timeout helper
    Sometimes we may want to limit certain boost kicks to only take effect
    within a certain timeout after receiving the last input event to save
    power. For example, we may want to boost when an intensive process
    forks to improve fluidity, but boosting when the fork was not initiated
    by input is wasteful.
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/devfreq/devfreq_boost.c
+++ drivers/devfreq/devfreq_boost.c
@@ -29,6 +29,7 @@ struct boost_dev {
 struct df_boost_drv {
 	struct boost_dev devices[DEVFREQ_MAX];
 	struct notifier_block fb_notif;
+	unsigned long last_input_jiffies;
 };
 
 static void devfreq_input_unboost(struct work_struct *work);
@@ -92,6 +93,14 @@ static void __devfreq_boost_kick_max(struct boost_dev *b,
 	if (!mod_delayed_work(system_unbound_wq, &b->max_unboost,
 			      boost_jiffies))
 		wake_up(&b->boost_waitq);
+}
+
+bool df_boost_within_input(unsigned long timeout_ms)
+{
+	struct df_boost_drv *d = &df_boost_drv_g;
+
+	return time_before(jiffies, d->last_input_jiffies +
+			   msecs_to_jiffies(timeout_ms));
 }
 
 void devfreq_boost_kick_max(enum df_device device, unsigned int duration_ms)
@@ -211,6 +220,8 @@ static void devfreq_boost_input_event(struct input_handle *handle,
 
 	for (i = 0; i < DEVFREQ_MAX; i++)
 		__devfreq_boost_kick(d->devices + i);
+
+	d->last_input_jiffies = jiffies;
 }
 
 static int devfreq_boost_input_connect(struct input_handler *handler,
@@ -304,6 +315,7 @@ static int __init devfreq_boost_init(void)
 		}
 	}
 
+	d->last_input_jiffies = jiffies;
 	devfreq_boost_input_handler.private = d;
 	ret = input_register_handler(&devfreq_boost_input_handler);
 	if (ret) {

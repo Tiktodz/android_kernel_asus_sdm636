ed1ec7a1d14a07113638d7b3a3bd884246b341bf
Sultan Alsawaf <sultan@kerneltoast.com>
2023-01-25 19:18:26 +0700

    kernel: Add API to mark IRQs and kthreads as performance critical
    On devices with a CPU that contains heterogeneous cores (e.g.,
    big.LITTLE), it can be beneficial to place some performance-critical
    IRQs and kthreads onto the performance CPU cluster in order to improve
    performance.
    
    This commit adds the following APIs:
    - kthread_run_perf_critical() to create and start a perf-critical kthread
    - IRQF_PERF_CRITICAL to schedule an IRQ and any threads it may have onto
      performance CPUs
    - PF_PERF_CRITICAL to mark a process (mainly a kthread) as performance
      critical (this is used by kthread_run_perf_critical())
    
    In order to accommodate this new API, the following changes are made:
    - Performance-critical IRQs are distributed evenly among online CPUs
      available in cpu_perf_mask
    - Performance-critical IRQs have their affinities reaffined upon exit
      from suspend (since the affinities are broken when non-boot CPUs are
      disabled)
    - Performance-critical IRQs and their threads have their affinities reset
      upon entering suspend, so that upon immediate suspend exit (when only
      the boot CPU is online), interrupts can be processed and interrupt
      threads can be scheduled onto an online CPU (otherwise we'd hit a
      kernel BUG)
    - do_set_cpus_allowed() and __set_cpus_allowed_ptr() are modified to
      enforce a performance-critical kthread's affinity
    - Perf-critical IRQs are marked with IRQD_AFFINITY_MANAGED so userspace
      can't mess with their affinity
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1206,6 +1206,8 @@ void do_set_cpus_allowed(struct task_struct *p, const struct cpumask *new_mask)
 	struct rq *rq = task_rq(p);
 	bool queued, running;
 
+	new_mask = adjust_cpumask(p, new_mask);
+
 	lockdep_assert_held(&p->pi_lock);
 
 	queued = task_on_rq_queued(p);
@@ -1246,6 +1248,10 @@ static int __set_cpus_allowed_ptr(struct task_struct *p,
 	struct rq *rq;
 	unsigned int dest_cpu;
 	int ret = 0;
+
+	/* Force all performance-critical kthreads onto the big cluster */
+	if (p->flags & PF_PERF_CRITICAL)
+		new_mask = cpu_perf_mask;
 
 	rq = task_rq_lock(p, &flags);
 

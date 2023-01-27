1ba1ff70be7db4ca540c1960a07c36fdac48d54e
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-01-27 14:53:54 +0700

    sensors: stk3x: Set flags = 1
    Correctly flag proximity sensor as wakeup sensor.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- drivers/sensors/stk3x3x/stk3x3x.c
+++ drivers/sensors/stk3x3x/stk3x3x.c
@@ -301,6 +301,7 @@ static struct sensors_classdev sensors_proximity_cdev = {
 	.min_delay = 0,
 	.fifo_reserved_event_count = 0,
 	.fifo_max_event_count = 0,
+	.flags = 1,
 	.enabled = 0,
 	.delay_msec = 200,
 	.sensors_enable = NULL,

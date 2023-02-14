ef86f59582d96095175145a9f4b20ae6dfeb03fc
LibXZR <xzr467706992@163.com>
2023-02-14 17:28:27 +0700

    fs: exec: Affine surfaceflinger to big CPUs
    Surfaceflinger can sometimes be very heavy. Affining on perf would lead
    to overload especially when tasks are upmigrating from big cluster.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- fs/exec.c
+++ fs/exec.c
@@ -73,6 +73,7 @@ static LIST_HEAD(formats);
 static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN "/vendor/bin/hw/android.hardware.graphics.composer"
+#define SURFACEFLINGER_BIN "/system/bin/surfaceflinger"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 
@@ -1660,6 +1661,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN,
 					   strlen(HWCOMPOSER_BIN)))) {
+			current->pf_flags |= PF_PERF_CRITICAL;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   SURFACEFLINGER_BIN,
+					   strlen(SURFACEFLINGER_BIN)))) {
 			current->pf_flags |= PF_PERF_CRITICAL;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}

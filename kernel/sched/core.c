7a63be8a39a13c18a7d8f9ee5278f15ef31fa737
wimbiyoashizkia <wimbiyoashizkia@yahoo.com>
2023-03-04 00:20:13 +0700

    Revert "ANDROID: cpufreq: track per-task time in state"
    This reverts commit fba21f6831a2c5507adb5f4d9e35f02e207ad18b.
    
    Signed-off-by: wimbiyoashizkia <wimbiyoashizkia@yahoo.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -77,7 +77,6 @@
 #include <linux/compiler.h>
 #include <linux/irq.h>
 #include <linux/sched/core_ctl.h>
-#include <linux/cpufreq_times.h>
 
 #include <asm/switch_to.h>
 #include <asm/tlb.h>
@@ -2366,10 +2365,6 @@ static void __sched_fork(unsigned long clone_flags, struct task_struct *p)
 
 #ifdef CONFIG_SCHEDSTATS
 	memset(&p->se.statistics, 0, sizeof(p->se.statistics));
-#endif
-
-#ifdef CONFIG_CPU_FREQ_TIMES
-	cpufreq_task_times_init(p);
 #endif
 
 	RB_CLEAR_NODE(&p->dl.rb_node);

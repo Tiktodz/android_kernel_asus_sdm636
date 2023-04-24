958e964299b04d3b75e4409fb27af474253279c4
RyuujiX <saputradenny712@gmail.com>
2023-04-24 19:41:14 +0700

    fs: Don't run ksu command when KernelSU is disabled
    * For this commit: https://github.com/RyuujiX/KernelSU/commit/2d9aebf64b30946ecb98c81bba1ebffdd64a8f2f
    
    Change-Id: I54b30726ed05b1a37d46dfffe5cac33620b3fa0c
    Signed-off-by: RyuujiX <saputradenny712@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- fs/read_write.c
+++ fs/read_write.c
@@ -21,6 +21,10 @@
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 
+#ifdef CONFIG_KSU
+#include <linux/ksu.h>
+#endif
+
 const struct file_operations generic_ro_fops = {
 	.llseek		= generic_file_llseek,
 	.read_iter	= generic_file_read_iter,
@@ -446,7 +450,8 @@ ssize_t vfs_read(struct file *file, char __user *buf, size_t count, loff_t *pos)
 	ssize_t ret;
 	
 #ifdef CONFIG_KSU
-	ksu_handle_vfs_read(&file, &buf, &count, &pos);
+	if (get_ksu_state() > 0)
+		ksu_handle_vfs_read(&file, &buf, &count, &pos);
 #endif
 
 	if (!(file->f_mode & FMODE_READ))

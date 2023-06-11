47802d0b7aaeb7c668c6d2947f4fedfde5819bf7
Kneba <abenkenary3@gmail.com>
2023-06-11 07:16:29 +0000

    power_supply: more fixing after 2072d12
    Signed-off-by: Kneba <abenkenary3@gmail.com>
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- drivers/power/power_supply_sysfs.c
+++ drivers/power/power_supply_sysfs.c
@@ -312,10 +312,10 @@ static struct device_attribute power_supply_attrs[] = {
 	POWER_SUPPLY_ATTR(enable_jeita_detection),
 	POWER_SUPPLY_ATTR(allow_hvdcp3),
 	POWER_SUPPLY_ATTR(max_pulse_allowed),
+	POWER_SUPPLY_ATTR(fg_reset_clock),
 	POWER_SUPPLY_ATTR(parallel_batfet_mode),
 	POWER_SUPPLY_ATTR(parallel_fcc_max),
 	POWER_SUPPLY_ATTR(min_icl),
-	POWER_SUPPLY_ATTR(fg_reset_clock),
 #ifdef CONFIG_MACH_ASUS_SDM660
 	POWER_SUPPLY_ATTR(adapter_id),
 #endif

0c5bd044d577e7fe0f969800708d123361cd0b70
Sultan Alsawaf <sultan@kerneltoast.com>
2023-09-07 19:51:24 +0800

    kernel: Introduce SBalance IRQ balancer (port Linux 4.4.y)
    This is a simple IRQ balancer that polls every X number of milliseconds and
    moves IRQs from the most interrupt-heavy CPU to the least interrupt-heavy
    CPUs until the heaviest CPU is no longer the heaviest. IRQs are only moved
    from one source CPU to any number of destination CPUs per balance run.
    Balancing is skipped if the gap between the most interrupt-heavy CPU and
    the least interrupt-heavy CPU is below the configured threshold of
    interrupts.
    
    The heaviest IRQs are targeted for migration in order to reduce the number
    of IRQs to migrate. If moving an IRQ would reduce overall balance, then it
    won't be migrated.
    
    The most interrupt-heavy CPU is calculated by scaling the number of new
    interrupts on that CPU to the CPU's current capacity. This way, interrupt
    heaviness takes into account factors such as thermal pressure and time
    spent processing interrupts rather than just the sheer number of them. This
    also makes SBalance aware of CPU asymmetry, where different CPUs can have
    different performance capacities and be proportionally balanced.
    
    Changes for Linux 4.4:
      Added <linux/kthread.h> which is later called by ../sched/sched.h
      Delete the const pointer of bal_irq_move_node_cmp
      refer to ("treewide: Change list_sort to use const pointers")
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Change-Id: I60544cdc9eb8ac751ccac22d23314c4e668e3cef
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- kernel/irq/irqdesc.c
+++ kernel/irq/irqdesc.c
@@ -162,6 +162,7 @@ static struct irq_desc *alloc_desc(int irq, int node, struct module *owner)
 	lockdep_set_class(&desc->lock, &irq_desc_lock_class);
 
 	desc_set_defaults(irq, desc, node, owner);
+	sbalance_desc_add(desc);
 
 	return desc;
 
@@ -176,6 +177,7 @@ static void free_desc(unsigned int irq)
 {
 	struct irq_desc *desc = irq_to_desc(irq);
 
+	sbalance_desc_del(desc);
 	unregister_irq_proc(irq, desc);
 
 	/*

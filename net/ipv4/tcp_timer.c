0a146075f3e3b9053cf5ba8f4c57dc5f633c5155
dotkit <ewprjkt@proton.me>
2024-01-11 19:21:48 +0000

    Revert "ipv4: tcp: Linear timeouts for thin streams"
    This reverts commit e24d5f54421bd699598ce4f27108c829f0735f5a.

--- net/ipv4/tcp_timer.c
+++ net/ipv4/tcp_timer.c
@@ -30,7 +30,7 @@ int sysctl_tcp_keepalive_intvl __read_mostly = TCP_KEEPALIVE_INTVL;
 int sysctl_tcp_retries1 __read_mostly = TCP_RETR1;
 int sysctl_tcp_retries2 __read_mostly = TCP_RETR2;
 int sysctl_tcp_orphan_retries __read_mostly;
-int sysctl_tcp_thin_linear_timeouts __read_mostly = 1;
+int sysctl_tcp_thin_linear_timeouts __read_mostly;
 
 /*Function to reset tcp_ack related sysctl on resetting master control */
 void set_tcp_default(void)

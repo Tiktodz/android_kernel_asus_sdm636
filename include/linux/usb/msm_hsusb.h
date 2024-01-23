5e07f6a6e7df269295ac4604f3a9fcb933139cc6
Kneba <abenkenary3@gmail.com>
2024-01-23 15:20:44 +0000

    msm_hsusb: Increase maximum charging current
    Change-Id: I13be010c971379de100b2950a9d876a1f9acf1ff
    Signed-off-by: dotkit <ewprjkt@proton.me>

--- include/linux/usb/msm_hsusb.h
+++ include/linux/usb/msm_hsusb.h
@@ -70,8 +70,10 @@ enum msm_usb_phy_type {
 	SNPS_28NM_INTEGRATED_PHY,
 };
 
-#define IDEV_CHG_MAX	2000
+#define IDEV_CHG_MAX	3000
 #define IUNIT		100
+
+#define IDEV_HVDCP_CHG_MAX	3000
 
 /**
  * Different states involved in USB charger detection.

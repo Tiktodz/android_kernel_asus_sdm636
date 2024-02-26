6c77302f49deb8d8b3d5c697bf47979e12398b9d
sandatjepil <masamik2222@gmail.com>
2024-02-27 05:25:53 +0700

    fs: exec: avoid dangling else
    fs/exec.c:1570:6: warning: add explicit braces to avoid dangling else [-Wdangling-else]
                else
                ^

--- fs/exec.c
+++ fs/exec.c
@@ -1555,11 +1555,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	int retval;
 	
 #ifdef CONFIG_KSU
-	if (get_ksu_state() > 0)
+	if (get_ksu_state() > 0) {
 	    if (unlikely(ksu_execveat_hook))
 		    ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
 	    else
 		    ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+	}
 #endif
 
 	if (IS_ERR(filename))

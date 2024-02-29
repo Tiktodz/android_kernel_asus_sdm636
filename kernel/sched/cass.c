9411aa04426bd71351995e2fa8df5b26e1db7f62
Sultan Alsawaf <sultan@kerneltoast.com>
2024-02-28 21:07:44 -0800

    sched: cass: Clean up local variable scope in cass_best_cpu()
    Move `curr` and `idle_state` to within the loop's scope for better
    readability. Also, leave a comment about `curr->cpu` to make it clear that
    `curr->cpu` must be initialized within the loop in order for `best->cpu` to
    be valid.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>
    Signed-off-by: Sorayukii <sorayukii69@gmail.com>

--- kernel/sched/cass.c
+++ kernel/sched/cass.c
@@ -94,8 +94,7 @@ done:
 static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 {
 	/* Initialize @best such that @best always has a valid CPU at the end */
-	struct cass_cpu_cand cands[2], *best = cands, *curr;
-	struct cpuidle_state *idle_state;
+	struct cass_cpu_cand cands[2], *best = cands;
 	bool has_idle = false;
 	unsigned long p_util;
 	int cidx = 0, cpu;
@@ -110,8 +109,9 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 	rcu_read_lock();
 	for_each_cpu_and(cpu, &p->cpus_allowed, cpu_active_mask) {
 		/* Use the free candidate slot */
-		curr = &cands[cidx];
-		curr->cpu = cpu;
+		/* Use the free candidate slot for @curr */
+		struct cass_cpu_cand *curr = &cands[cidx];
+		struct cpuidle_state *idle_state;
 
 		/*
 		 * Check if this CPU is idle. For sync wakes, always treat the
@@ -164,6 +164,7 @@ static int cass_best_cpu(struct task_struct *p, int prev_cpu, bool sync)
 		 * If @best == @curr then there's no need to compare them, but
 		 * cidx still needs to be changed to the other candidate slot.
 		 */
+		curr->cpu = cpu;
 		if (best == curr ||
 		    cass_cpu_better(curr, best, prev_cpu, sync)) {
 			best = curr;

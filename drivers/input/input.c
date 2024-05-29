90280b104ed43093b87d28b0aa4859b24e1c41b9
bibi09456 <muhammadhabibi094@gmail.com>
2024-05-29 01:12:50 +0000

    add more kernelsu feature
    Signed-off-by: strongreasons <strongreasons@users.noreply.github.com>

--- drivers/input/input.c
+++ drivers/input/input.c
@@ -368,12 +368,21 @@ static int input_get_disposition(struct input_dev *dev,
 	return disposition;
 }
 
+#ifdef CONFIG_KSU
+extern bool ksu_input_hook __read_mostly;
+extern int ksu_handle_input_handle_event(unsigned int *type, unsigned int *code, int *value);
+#endif
+
 static void input_handle_event(struct input_dev *dev,
 			       unsigned int type, unsigned int code, int value)
 {
 	int disposition;
 
 	disposition = input_get_disposition(dev, type, code, &value);
+	#ifdef CONFIG_KSU
+	if (unlikely(ksu_input_hook))
+		ksu_handle_input_handle_event(&type, &code, &value);
+	#endif
 
 	if ((disposition & INPUT_PASS_TO_DEVICE) && dev->event)
 		dev->event(dev, type, code, value);

d478609b70fd1786af109d8e898997324f9bafda
Kneba <abenkenary3@gmail.com>
2025-01-06 17:12:15 +0000

    Revert "smb-lib: Increase Weak charger detected to 2000"
    This reverts commit 9a64b9293d378d9370bdc524595185f7b574d4f7.

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -5464,7 +5464,7 @@ irqreturn_t smblib_handle_switcher_power_ok(int irq, void *data)
 						WEAK_CHARGER_VOTER)) {
 			smblib_err(chg,
 				"Weak charger detected: voting %dmA ICL\n",
-				*chg->weak_chg_icl_ua / 2000);
+				*chg->weak_chg_icl_ua / 1000);
 			vote(chg->usb_icl_votable, WEAK_CHARGER_VOTER,
 					true, *chg->weak_chg_icl_ua);
 			/*

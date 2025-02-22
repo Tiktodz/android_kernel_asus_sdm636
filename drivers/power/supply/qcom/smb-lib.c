cd0b57aad5cbc9f695a8d3c7bf8e0000a53bc95b
Agus Suristyono <sandatjepil@proton.me>
2025-02-23 00:13:06 +0700

    drivers: power: increase DCP_CURRENT_UA to 2500000 uA
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -2752,7 +2752,7 @@ int smblib_get_prop_die_health(struct smb_charger *chg,
 #define SDP_CURRENT_UA			500000
 #define CDP_CURRENT_UA			1500000
 #ifdef CONFIG_MACH_ASUS_SDM660
-#define DCP_CURRENT_UA			2000000
+#define DCP_CURRENT_UA			2500000
 #else
 #define DCP_CURRENT_UA			2100000
 #endif

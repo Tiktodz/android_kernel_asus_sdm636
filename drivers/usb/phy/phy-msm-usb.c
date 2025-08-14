d081b78746bddec378ce25aa898924ce95b81bfb
texascake <tiktodz@soulvibe-team.cloud>
2025-08-14 17:25:42 +0000

    drivers: usb: Increase charge current limit
    Change-Id: If7dfd4d695fdbe0dfdb764ba7231016c54f86f69
    Signed-off-by: texascake <tiktodz@soulvibe-team.cloud>

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -46,6 +46,10 @@
 #include <linux/usb/msm_hsusb_hw.h>
 #include <linux/regulator/consumer.h>
 
+#ifdef CONFIG_FORCE_FAST_CHARGE
+#include <linux/fastchg.h>
+#endif
+
 #define MSM_USB_BASE	(motg->regs)
 #define DRIVER_NAME	"msm_otg"
 
@@ -660,6 +664,15 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 {
 	if (motg->cur_power == mA)
 		return;
+
+#ifdef CONFIG_FORCE_FAST_CHARGE
+	if (force_fast_charge > 0 && mA > 0) {
+		mA = IDEV_CHG_MAX;
+		pr_info("USB fast charging is ON\n");
+	} else {
+		pr_info("USB fast charging is OFF\n");
+	}
+#endif
 
 	/* TODO: Notify PMIC about available current */
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);

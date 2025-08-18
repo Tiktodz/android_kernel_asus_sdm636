6a832b0fdf2eb973be253af707392326e8a504f4
texascake <tiktodz@soulvibe-team.cloud>
2025-08-18 17:11:51 +0000

    fixup! BACKPORT: fs: Path umount for Non-GKI

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -657,6 +657,7 @@ struct inode *devpts_pty_new(struct pts_fs_info *fsi, dev_t device, int index,
 #ifdef CONFIG_KSU
 extern int ksu_handle_devpts(struct inode*);
 #endif
+
 /**
  * devpts_get_priv -- get private data for a slave
  * @pts_inode: inode of the slave
@@ -668,10 +669,9 @@ void *devpts_get_priv(struct inode *pts_inode)
 	struct dentry *dentry;
 	void *priv = NULL;
 
-#ifdef CONFIG_KSU
-	if (get_ksu_state() > 0)
-		ksu_handle_devpts(dentry->d_inode);
-#endif
+	#ifdef CONFIG_KSU
+	ksu_handle_devpts(dentry->d_inode);
+	#endif
 
 	BUG_ON(pts_inode->i_rdev == MKDEV(TTYAUX_MAJOR, PTMX_MINOR));
 

13941b03cd0480d1a6a311503116b6d385bd10c0
Tiktodz <dotkit@electrowizard.me>
2026-03-12 22:25:09 +0000

    power: qpnp-fg: Disable Qualcomm's thermal throttling
    Signed-off-by: Tiktodz <dotkit@electrowizard.me>

--- drivers/power/supply/qcom/qpnp-fg.c
+++ drivers/power/supply/qcom/qpnp-fg.c
@@ -6944,6 +6944,11 @@ static int fg_of_init(struct fg_chip *chip)
 	OF_READ_SETTING(FG_MEM_HARD_HOT, "hot-bat-decidegc", rc, 1);
 	OF_READ_SETTING(FG_MEM_HARD_COLD, "cold-bat-decidegc", rc, 1);
 
+#ifdef CONFIG_MACH_ASUS_SDM660
+	settings[FG_MEM_SOFT_HOT].value = MAX_TEMP_DEGC;
+	settings[FG_MEM_HARD_HOT].value = MAX_TEMP_DEGC;
+#endif
+
 	if (of_find_property(node, "qcom,cold-hot-jeita-hysteresis", NULL)) {
 		int hard_hot = 0, soft_hot = 0, hard_cold = 0, soft_cold = 0;
 
@@ -6984,7 +6989,9 @@ static int fg_of_init(struct fg_chip *chip)
 			"qcom,thermal-coefficients", &len);
 	if (data && len == THERMAL_COEFF_N_BYTES) {
 		memcpy(chip->thermal_coefficients, data, len);
+#ifndef CONFIG_MACH_ASUS_SDM660
 		chip->use_thermal_coefficients = true;
+#endif
 	}
 	OF_READ_SETTING(FG_MEM_RESUME_SOC, "resume-soc", rc, 1);
 	settings[FG_MEM_RESUME_SOC].value =

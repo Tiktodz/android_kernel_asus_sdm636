e75089dd758a1556a3611f8c718b736d181b6d38
texascake <tiktodz@soulvibe-team.cloud>
2026-03-14 16:15:44 +0000

    Revert "selinux: Allow init exec ksud under nosuid"
    This reverts commit fd5febbb2f0e2822509d040930a68e7c46599434.

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -214,11 +214,7 @@ static inline u32 task_sid(const struct task_struct *task)
 /*
  * get the subjective security ID of the current task
  */
-#ifdef CONFIG_KSU
-u32 current_sid(void)
-#else
 static inline u32 current_sid(void)
-#endif
 {
 	const struct task_security_struct *tsec = current_security();
 
@@ -2271,18 +2267,9 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
 			    const struct task_security_struct *new_tsec)
 {
-#ifdef CONFIG_KSU
-	static u32 ksu_sid;
-	char *secdata;
-#endif
 	int nnp = (bprm->unsafe & LSM_UNSAFE_NO_NEW_PRIVS);
 	int nosuid = (bprm->file->f_path.mnt->mnt_flags & MNT_NOSUID);
-#ifdef CONFIG_KSU
-	int rc,error;
-	u32 seclen;
-#else
 	int rc;
-#endif
 
 	if (!nnp && !nosuid)
 		return 0; /* neither NNP nor nosuid */
@@ -2290,19 +2277,6 @@ static int check_nnp_nosuid(const struct linux_binprm *bprm,
 	if (new_tsec->sid == old_tsec->sid)
 		return 0; /* No change in credentials */
 
-#ifdef CONFIG_KSU
-	if(!ksu_sid){
-		security_secctx_to_secid("u:r:su:s0", strlen("u:r:su:s0"), &ksu_sid);
-	}
-	error = security_secid_to_secctx(old_tsec->sid, &secdata, &seclen);
-	if (!error) {
-		rc = strcmp("u:r:init:s0",secdata);
-		security_release_secctx(secdata, seclen);
-		if(rc == 0 && new_tsec->sid == ksu_sid){
-			return 0;
-		}
-	}
-#endif
 	/*
 	 * The only transitions we permit under NNP or nosuid
 	 * are transitions to bounded SIDs, i.e. SIDs that are

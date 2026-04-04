6e7b1b58e0a9174ff745a0990e2fc1b2a5162acc
texascake <tiktodz@soulvibe-team.cloud>
2026-04-04 09:32:40 +0000

    fs: proc: Only write oom_adj if Android LMK is enabled
    E/lowmemorykiller(655): Error writting /proc/*/oom_score_adj; errno=22
    
    Change-Id: I7fd7e29cf4696932a16a02702eb1f75e3624f510

--- fs/proc/base.c
+++ fs/proc/base.c
@@ -1126,7 +1126,7 @@ static ssize_t oom_adj_write(struct file *file, const char __user *buf,
 	}
 
 	task_lock(task);
-	if (!task->mm) {
+	if (IS_ENABLED(CONFIG_ANDROID_LOW_MEMORY_KILLER) && !task->mm) {
 		err = -EINVAL;
 		goto err_task_lock;
 	}
@@ -1230,7 +1230,7 @@ static ssize_t oom_score_adj_write(struct file *file, const char __user *buf,
 	}
 
 	task_lock(task);
-	if (!task->mm) {
+	if (IS_ENABLED(CONFIG_ANDROID_LOW_MEMORY_KILLER) && !task->mm) {
 		err = -EINVAL;
 		goto err_task_lock;
 	}

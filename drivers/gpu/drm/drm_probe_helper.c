befc9a13d7000b3f377f1282e46725ba4b0a88e2
texascake <tiktodz@soulvibe-team.cloud>
2026-04-09 10:28:48 +0000

    gpu: drm: Use power efficient working queues
    Co-authored-by: freak07 <michalechner92@googlemail.com>
    Change-Id: I2affd22a706c12d116124ea4065b1b2702acbf23

--- drivers/gpu/drm/drm_probe_helper.c
+++ drivers/gpu/drm/drm_probe_helper.c
@@ -122,7 +122,8 @@ void drm_kms_helper_poll_enable_locked(struct drm_device *dev)
 	}
 
 	if (poll)
-		schedule_delayed_work(&dev->mode_config.output_poll_work, DRM_OUTPUT_POLL_PERIOD);
+		queue_delayed_work(system_power_efficient_wq,
+							&dev->mode_config.output_poll_work, delay);
 }
 EXPORT_SYMBOL(drm_kms_helper_poll_enable_locked);
 
@@ -180,8 +181,8 @@ static int drm_helper_probe_single_connector_modes_merge_bits(struct drm_connect
 			 */
 			dev->mode_config.delayed_event = true;
 			if (dev->mode_config.poll_enabled)
-				schedule_delayed_work(&dev->mode_config.output_poll_work,
-						      0);
+			    queue_delayed_work(system_power_efficient_wq, 
+								&dev->mode_config.output_poll_work, 0);
 		}
 	}
 
@@ -399,7 +400,8 @@ out:
 		drm_kms_helper_hotplug_event(dev);
 
 	if (repoll)
-		schedule_delayed_work(delayed_work, DRM_OUTPUT_POLL_PERIOD);
+		queue_delayed_work(system_power_efficient_wq, delayed_work, 
+							DRM_OUTPUT_POLL_PERIOD);
 }
 
 /**

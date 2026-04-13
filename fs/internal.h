97dc4a437bdd0913c609dc6013e24ea2365c631b
texascake <tiktodz@soulvibe-team.cloud>
2026-04-13 17:05:26 +0000

    vfs: add path_umount() helper
    This commit extracts the struct path-based unmounting logic from the existing umount syscall code
    into a helper function for reuse by other parts of the kernel.
    
    [1]
    https://github.com/KernelSU-Next/KernelSU-Next/issues/467
    
    Change-Id: I69389d83ddaa8308d15d4c6c5a8f8fb3b6481651

--- fs/internal.h
+++ fs/internal.h
@@ -70,6 +70,8 @@ extern int __mnt_want_write_file(struct file *);
 extern void __mnt_drop_write(struct vfsmount *);
 extern void __mnt_drop_write_file(struct file *);
 
+int path_umount(struct path *path, int flags);
+
 /*
  * fs_struct.c
  */

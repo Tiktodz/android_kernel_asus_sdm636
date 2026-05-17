ddafd9778c9481ad33d754bb58d789950fa8649a
texascake <tiktodz@soulvibe-team.cloud>
2026-05-17 13:50:08 +0000

    Revert "thermal: lmh_lite: Separate polling support from LMH Lite"
    This reverts commit d70f742e7bd7b2b0abdaae2b04db78c0ee9e615d.

--- drivers/thermal/lmh_lite.c
+++ drivers/thermal/lmh_lite.c
@@ -371,7 +371,6 @@ read_exit:
 	return;
 }
 
-#ifdef CONFIG_LIMITS_LITE_HW_POLLING
 static void lmh_poll(struct work_struct *work)
 {
 	struct lmh_driver_data *lmh_dat = container_of(work,
@@ -396,7 +395,6 @@ poll_exit:
 	up_write(&lmh_sensor_access);
 	return;
 }
-#endif
 
 static void lmh_trim_error(void)
 {
@@ -1302,7 +1300,6 @@ static int lmh_probe(struct platform_device *pdev)
 	}
 	lmh_data->dev = &pdev->dev;
 
-#ifdef CONFIG_LIMITS_LITE_HW_POLLING
 	lmh_data->poll_wq = alloc_workqueue("lmh_poll_wq", WQ_HIGHPRI, 0);
 	if (!lmh_data->poll_wq) {
 		pr_err("Error allocating workqueue\n");
@@ -1310,7 +1307,6 @@ static int lmh_probe(struct platform_device *pdev)
 		goto probe_exit;
 	}
 	INIT_DEFERRABLE_WORK(&lmh_data->poll_work, lmh_poll);
-#endif
 
 	ret = lmh_sensor_init(pdev);
 	if (ret) {
@@ -1334,9 +1330,7 @@ static int lmh_probe(struct platform_device *pdev)
 	}
 	platform_set_drvdata(pdev, lmh_data);
 
-#ifndef CONFIG_LIMITS_LITE_HW_POLLING
 	return ret;
-#endif
 
 probe_exit:
 	if (lmh_data->poll_wq)

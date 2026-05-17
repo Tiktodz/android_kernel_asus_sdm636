3e75919fbb222c65c5d3b69c5a46467647e7eaea
texascake <tiktodz@soulvibe-team.cloud>
2026-05-17 13:48:33 +0000

    Revert "cmdline: Replace for SafetyNet CTS pass"
    This reverts commit 97fd46968a31770af23cb9633d11b699cf110579.

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -24,55 +24,27 @@ static const struct file_operations cmdline_proc_fops = {
 	.release	= single_release,
 };
 
-static char *padding = " ";
-
-static void replace_flag(char *cmd, const char *flag, const char *flag_new)
+static void patch_flag(char *cmd, const char *flag, const char *val)
 {
-	char *start_addr, *end_addr;
+	size_t flag_len, val_len;
+	char *start, *end;
 
-	/* Ensure all instances of a flag are replaced */
-	while ((start_addr = strstr(cmd, flag))) {
-		end_addr = strchr(start_addr, ' ');
-		if (end_addr) {
-			if (strlen(flag)<strlen(flag_new)) {
-				/*
-				 * xx yy=a zz
-				 *    ^   ^
-				 * xx yy=bb zz
-				 */
+	start = strstr(cmd, flag);
+	if (!start)
+		return;
 
-				/* +1 to copy trailing '/0' */
-				int length_to_copy = strlen( start_addr + (strlen(flag) ) ) + 1;
-				int length_diff = strlen(flag_new)-strlen(flag);
-				memcpy(start_addr+(strlen(flag)+length_diff), start_addr+(strlen(flag)), length_to_copy);
-				memcpy(start_addr+(strlen(flag)), padding, length_diff);
-			}
-			memcpy(start_addr, flag_new, strlen(flag_new));
-		}
-		else
-			*(start_addr - 1) = '\0';
-	}
+	flag_len = strlen(flag);
+	val_len = strlen(val);
+	end = start + flag_len + strcspn(start + flag_len, " ");
+	memmove(start + flag_len + val_len, end, strlen(end) + 1);
+	memcpy(start + flag_len, val, val_len);
 }
 
-static void replace_safetynet_flags(char *cmd)
+static void patch_safetynet_flags(char *cmd)
 {
-	/* 
-	 * WARNING: be aware that you can't replace 
-	 * shorter string with longer ones in 
-	 * the function called here...
-	 */
-	replace_flag(cmd, "androidboot.vbmeta.device_state=unlocked",
-			  "androidboot.vbmeta.device_state=locked  ");
-	replace_flag(cmd, "androidboot.enable_dm_verity=0",
-			  "androidboot.enable_dm_verity=1");
-	replace_flag(cmd, "androidboot.secboot=disabled",
-			  "androidboot.secboot=enabled ");
-	replace_flag(cmd, "androidboot.verifiedbootstate=orange",
-			  "androidboot.verifiedbootstate=green ");
-	replace_flag(cmd, "androidboot.veritymode=logging",
-			  "androidboot.veritymode=enforcing");
-	replace_flag(cmd, "androidboot.veritymode=eio",
-			  "androidboot.veritymode=enforcing");
+	patch_flag(cmd, "androidboot.flash.locked=", "1");
+	patch_flag(cmd, "androidboot.verifiedbootstate=", "green");
+	patch_flag(cmd, "androidboot.veritymode=", "enforcing");
 }
 
 static int __init proc_cmdline_init(void)
@@ -80,11 +52,11 @@ static int __init proc_cmdline_init(void)
 	strcpy(new_command_line, saved_command_line);
 
 	/*
-	 * Replace various flags from command line seen by userspace in order to
-	 * pass SafetyNet CTS check.
+	 * Patch various flags from command line seen by userspace in order to
+	 * pass SafetyNet checks.
 	 */
-	replace_safetynet_flags(new_command_line);
-
+	patch_safetynet_flags(new_command_line);
+	
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }

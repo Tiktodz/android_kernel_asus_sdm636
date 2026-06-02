e088c8b28aaf7ceba117eecfd1e8bdaec2703891
Tiktodz <dotkit@electrowizard.me>
2026-06-02 06:29:49 +0000

    msm: mdss: Don't allow boost CPU and DDR after input timeout
    Change-Id: I6914af0d70d78f3d64947c56cfc7f21aa869cd2f

--- drivers/video/fbdev/msm/mdss_fb.c
+++ drivers/video/fbdev/msm/mdss_fb.c
@@ -5241,8 +5241,11 @@ int mdss_fb_do_ioctl(struct fb_info *info, unsigned int cmd,
 		ret = mdss_fb_mode_switch(mfd, dsi_mode);
 		break;
 	case MSMFB_ATOMIC_COMMIT:
-		cpu_input_boost_kick();
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
+		if (cpu_input_boost_within_input(3000))
+			cpu_input_boost_kick();
+
+		if (df_boost_within_input(3000))
+			devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
 		ret = mdss_fb_atomic_commit_ioctl(info, argp, file);
 		break;
 
